c656bdfe342bd1247530c94f82eb230b688724be
MafiaAlien <49702823+MafiaAlien@users.noreply.github.com>
2022-10-30 20:16:07 -0700

    feat

--- Q450Recur.py
+++ Q450Recur.py
@@ -1,10 +1,9 @@
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
         # did not find the key value in tree
@@ -38,10 +37,9 @@ class Solution:
                     # if current node's left child is None, assign cur's left child by target node' left 
                     cur.left = root.left 
                 return root.right 
-        
-        root.left = self.deleteNode(root.left, key)
-        root.right = self.deleteNode(root.right, key)
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        else:
+            root.right = self.deleteNode(root.right, key)
         return root 
-
-if __name__ == "__main__":
-    pass 
+        
