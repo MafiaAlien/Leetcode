1d81ec6758f14d492566345096cf3ae374cea577
MafiaAlien <49702823+MafiaAlien@users.noreply.github.com>
2023-09-12 14:16:20 -0700

    delete node func

--- LinkListCycle.go
+++ LinkListCycle.go
@@ -1,11 +1,5 @@
 package main
 
-// Definition for singly-linked list.
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
-
 func hasCycle(head *ListNode) bool {
 	if head == nil || head.Next == nil {
 		return false
